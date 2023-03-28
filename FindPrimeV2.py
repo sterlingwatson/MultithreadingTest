b72d43726fb477eae15ceaa48b7fa1f10bb7a3c7
sterling <sterling@Ubuntu.myguest.virtualbox.org>
2023-03-28 14:23:05 -0500

    updated, but buggy Version 2

--- FindPrimeV2.py
+++ FindPrimeV2.py
@@ -3,8 +3,8 @@ import multiprocessing
 import math
 
 #n = 558533
-#n = 483463113173934329
-n=13
+n = 483463113173934329
+#n=13
 sqrt_n = int(math.ceil(math.sqrt(n)))
 
 def is_prime(n):
@@ -22,38 +22,38 @@ def check_for_prime_in_range(start, end, check):
         if is_prime(n):
             check.append(n)
 
-
-with open("Times.txt", "a") as file:
-    for y in range(5):
-        start_time = time.time()
+if __name__ == '__main__':
+    with open("Times.txt", "a") as file:
+        for y in range(5):
+            start_time = time.time()
         
-        for j in range(number_of_threads):
-            start = j * (math.ceil(n/ number_of_threads))
-            end = (j+1) * (math.ceil(n/number_of_threads))
-            if j == number_of_threads - 1:
-                end = n
-            process = multiprocessing.Process(target = check_for_prime_in_range, args =  (start, end, results))
-            process.start()
+            for j in range(number_of_threads):
+                start = j * (math.ceil(n/ number_of_threads))
+                end = (j+1) * (math.ceil(n/number_of_threads))
+                if j == number_of_threads - 1:
+                    end = n
+                process = multiprocessing.Process(target = check_for_prime_in_range, args =  (start, end, results))
+                process.start()
 
         #wait for children to die
-        for p in  multiprocessing.active_children():
-            process.join()
+            for p in  multiprocessing.active_children():
+                process.join()
 
-        for r in results:
-            answers.append(r)
-            #print(f"{answers}")
+            for r in results:
+                answers.append(r)
+                #print(f"{answers}")
 
-    if n in answers or is_prime(n): #this is dumb, but I had constant bugs around it not including n in answers
+        if n in answers or is_prime(n): #this is dumb, but I had constant bugs around it not including n in answers
             print(f"{n} is a prime number")
-    else:
+        else:
             print(f"{n} is not a prime number")
             
-    time_taken = time.time() - start_time
-    print(f"Time: {time_taken:.2f} seconds")
-    #file.write(f"Run {y+1}, {time_taken:.2f} seconds, Version 2 with 1 Processor and 2 threads\n")
+        time_taken = time.time() - start_time
+        print(f"Time: {time_taken:.2f} seconds")
+        #file.write(f"Run {y+1}, {time_taken:.2f} seconds, Version 2 with 1 Processor and 2 threads\n")
 
-    #file.write(f"Run {y+1}, {time_taken:.2f} seconds, Version 2 with 2 Processor and 2 threads\n")
-    #file.write(f"Run {y+1}, {time_taken:.2f} seconds, Version 2 with 3 Processor and 3 threads\n")
+        #file.write(f"Run {y+1}, {time_taken:.2f} seconds, Version 2 with 2 Processor and 2 threads\n")
+        #file.write(f"Run {y+1}, {time_taken:.2f} seconds, Version 2 with 3 Processor and 3 threads\n")
 
 
 file.close()
