b8054bca2c9583c0acc215326ca67a9b65aed175
sterling <sterling@Ubuntu.myguest.virtualbox.org>
2023-04-02 12:28:45 -0500

    Commit using pool, version sent to prof for guidance

--- FindPrimeV1.py
+++ FindPrimeV1.py
@@ -22,7 +22,7 @@ with open("Times.txt", "a") as file:
 
         time_taken = time.time() - start_time
         print(f"Time: {time_taken:.2f} seconds")
-        file.write(f"Run {i+1}, {time_taken:.2f} seconds, Version 1 with 1 Processor\n")
+        #file.write(f"Run {i+1}, {time_taken:.2f} seconds, Version 1 with 1 Processor\n")
 
 
 file.close()
