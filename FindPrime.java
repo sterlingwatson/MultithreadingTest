33cad25305d0b969d06865afe2f51ea67cbed744
Sterling Watson <smwatso1@uno.edu>
2023-04-07 16:15:20 -0500

    Everything looks right, seconds work, pushing it over to the VM

--- FindPrime.java
+++ FindPrime.java
@@ -1,5 +1,5 @@
-import java.util.concurrent.TimeUnit;
 import java.math.BigInteger;
+import java.io.*;
 
 public class FindPrime {
     
@@ -39,43 +39,59 @@ public class FindPrime {
     }
     
     public static void main(String[] args) {
+        String fileName = "RunTimes.txt";
+
         String input = "2135729";
         BigInteger two = BigInteger.TWO;
         BigInteger three = BigInteger.TWO.add(BigInteger.ONE);
         BigInteger n = new BigInteger(input);
         BigInteger midpoint = n.divide(two);
 
+
+
         boolean result1 = false;
         boolean result2 = false;
+
+        try{
+
+            FileWriter fw = new FileWriter(fileName, true);
+            BufferedWriter buffer = new BufferedWriter(fw);
         
-        for (int i = 0; i < 5; i++) {
-            long startTime = System.nanoTime();
+            for (int i = 0; i < 5; i++) {
+                long startTime = System.nanoTime();
             
-            ThreadingData data1 = new ThreadingData(two, midpoint);
-            ThreadingData data2 = new ThreadingData(midpoint, n);
-            Thread t1 = new Thread(new FindPrimes(data1));
-            Thread t2 = new Thread(new FindPrimes(data2));
-            t1.start();
-            t2.start();
+                ThreadingData data1 = new ThreadingData(two, midpoint);
+                ThreadingData data2 = new ThreadingData(midpoint, n);
+                Thread t1 = new Thread(new FindPrimes(data1));
+                Thread t2 = new Thread(new FindPrimes(data2));
+                t1.start();
+                t2.start();
             
-            try {
-                t1.join();
-                t2.join();
-                result1 = data1.result;
-                result2 = data2.result;
+                try {
+                    t1.join();
+                    t2.join();
+                    result1 = data1.result;
+                    result2 = data2.result;
 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
-            if (result1 && result2) {
-                System.out.println(n + " is a prime number");
-            } else {
-                System.out.println(n + " is not a prime number");
-            }
+                if (result1 && result2) {
+                    System.out.println(n + " is a prime number");
+                } else {
+                    System.out.println(n + " is not a prime number");
+                }
             
-            double timeTaken = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startTime);
-            System.out.printf("Time: %.2f seconds\n", timeTaken);
+                float timeTaken = ((System.nanoTime() - startTime));
+                double timeInSeconds = timeTaken/1000000000;
+                String timeString =  String.format("Time: %.2f seconds, Run: %d with 1 Processor and 2 Thread", timeInSeconds, i);
+                System.out.printf("Time: %.2f seconds\n", timeInSeconds);
+                buffer.write(timeString);
+            }
+            buffer.close();
+        }catch (IOException e) {
+            System.out.println("An error occurred while trying to append the content to the file: " + e.getMessage());
         }
     }
 }
--- FindPrimeSingleThread.java
+++ FindPrimeSingleThread.java
@@ -1,5 +1,5 @@
-import java.util.concurrent.TimeUnit;
 import java.math.BigInteger;
+import java.io.*;
 
 public class FindPrimeSingleThread {
     
@@ -16,26 +16,41 @@ public class FindPrimeSingleThread {
     }
         
     public static void main(String[] args) {
+        String fileName = "RunTimes.txt";
+
         String input = "2135729";
         BigInteger two = BigInteger.TWO;
         BigInteger n = new BigInteger(input);
 
-        boolean result = false;
+        try{
+
+            FileWriter fw = new FileWriter(fileName, true);
+            BufferedWriter buffer = new BufferedWriter(fw);
+
+            boolean result = false;
         
-        for (int i = 0; i < 5; i++) {
-            long startTime = System.nanoTime();
+            for (int i = 0; i < 5; i++) {
+                long startTime = System.nanoTime();
             
-            result = isPrime(two, n);
+                result = isPrime(two, n);
             
-            if (result) {
-                System.out.println(n + " is a prime number");
-            } else {
-                System.out.println(n + " is not a prime number");
+                if (result) {
+                    System.out.println(n + " is a prime number");
+                } else {
+                   System.out.println(n + " is not a prime number");
+                }
+
+                float timeTaken = ((System.nanoTime() - startTime));
+                double timeInSeconds = timeTaken/1000000000;
+                String timeString =  String.format("Time: %.2f seconds, Run: %d with 1 Processor and 1 Thread", timeInSeconds, i);
+                System.out.printf("Time: %.2f seconds\n", timeInSeconds);
+                buffer.write(timeString);
             }
-            
-            double timeTaken = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startTime);
-            System.out.printf("Time: %.2f seconds\n", timeTaken);
+            buffer.close();
+        }catch (IOException e) {
+            System.out.println("An error occurred while trying to append the content to the file: " + e.getMessage());
         }
+        
     }
 }
 
